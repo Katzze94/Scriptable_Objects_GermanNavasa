6f34080c598ae6b556d8c2860d127c5d04be050c
Katzze94 <germannavasa2003@gmail.com>
2025-03-24 12:54:40 +0100

    Commit_Inventario

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -31,11 +31,35 @@ public class InventorySlot : MonoBehaviour
     {
         if(slotItem != null && !inspectionWindow.activeInHierarchy)
         {
+            deleteButton.onClick.AddListener(RemoveItem);
+            closeButton.onClick.AddListener(CloseWindow);
+
             inspectionImage.sprite = slotItem.itemSprite;
             inspectionName.text = slotItem.itemName;
             inspectionDescription.text = slotItem.itemDescription;
 
             inspectionWindow.SetActive(true);
         }
+    }
+
+     void RemoveItem()
+    {
+    
+    if(inventoryManager.isntance.weapons[slotNumber] !=null)
+    {
+        inventoryManager.isntance.weapons[slotNumber] = null;
+        inventoryManager.isntance.weaponsNames[slotNumber].text = "Empty";
+        inventoryManager.isntance.weaponsSprites[slotNumber].sprite = null;
+    }
+
+    CloseWindow();
+    
+    }
+
+    void CloseWindow()
+    {
+       deleteButton.onClick.RemoveListener(RemoveItem);
+        inspectionWindow.SetActive(false);
+
     }
 }
